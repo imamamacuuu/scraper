56ff9c44e98b6241384499e63e6b75b519ad64a2
imamamacuuu <110008354+imamamacuuu@users.noreply.github.com>
2022-07-27 19:18:38 +0700

    Update google-image.ts

--- src/images/google-image.ts
+++ src/images/google-image.ts
@@ -4,6 +4,8 @@ import got from 'got'
 export default async function googleImage (query: string): Promise<string[]> {
   const data = await got(`https://www.google.com/search?q=${query}&tbm=isch`, {
     headers: {
+	    cookie:
+	    'NID="511=Yt9NkK2-LnJ_JOEGQ-PGXEVuGs9-kkieJwyi08PhOQZSS71bz4C2AjUlIGQ3uxh2pY0Fll7wjlfOHYVKH45LKx7wX5FxeTEX8f0_RCssqbUcb9q2ZffSSbOCIsE3TVnGTx_g-Tuy6ideKPDK2Wn2x7Pn10pfACJGZYWc8k32R01moKNuv0RqjyhiWZLQ5BjbKIXygg_ZvjL-SB4ZCPR-yVHWVUU2nAQOiThI0LfXwJI_Q9P2kw"',
       accept:
 				'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
       'accept-encoding': 'gzip, deflate, br',
